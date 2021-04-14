d6b2be94f20d65b8de45410525c8359fab505928
Seiji Ralph Villafranca <seijiv13@outlook.com>
2021-04-14 12:06:21 +0800

    fix run command

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ["http://locahost:4200"]});
   await app.listen(process.env.PORT || 3000);
+  app.use(helmet());
 }
 bootstrap();
